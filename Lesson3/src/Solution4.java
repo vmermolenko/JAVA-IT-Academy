033f4296215aec8db4ecbdaf8a983d81aadbdeff
vmermolenko <43680550+vmermolenko@users.noreply.github.com>
2018-10-05 16:06:08 +0300

    Add files via upload

--- Lesson3/src/Solution4.java
+++ Lesson3/src/Solution4.java
@@ -1,3 +1,18 @@
+/**
+/Объявить 2 массива: целого и вещественного типа.
+//Посчитать сумму элементов к каждом массиве, 
+//вывести на экран.
+//Разделить каждый элемент массива целого типа
+//на максимальный элемент. 
+//Разделить каждый элемент массива вещественного типа
+//на минимальный элемент.
+
+//*Поменять местами максимальный элемент 
+//массива вещественного типа с минимальным
+//элементом массива целого типа.
+// Прирешении задачи создать не менее 10 методов
+ */
+
 
 public class Solution4 {
 
@@ -10,7 +25,20 @@ public class Solution4 {
 
 		summa(arr1);
 		summa(arr2);
+		//double max1 = max(arr1);
+		System.out.println("max(arr1)" + max(arr1));
+		//divider(arr1, max(arr1));
+		
+		System.out.println("max(arr2)" + max(arr2));
+		//divider(arr2, max(arr2));
+		
+		change(arr1, arr2); 
+		
+		printArray(arr1);
+		printArray(arr2);
 	}
+	
+
 
 	public static void summa(int[] arr) {
 		double s = 0;
@@ -37,8 +65,8 @@ public class Solution4 {
 		double max = arr[0];
 
 		for (int i = 0; i < arr.length; i++) {
-			if (max < arr[0]) {
-				max = arr[0];
+			if (max < arr[i]) {
+				max = arr[i];
 			}
 		}
 
@@ -50,8 +78,8 @@ public class Solution4 {
 		int max = arr[0];
 
 		for (int i = 0; i < arr.length; i++) {
-			if (max < arr[0]) {
-				max = arr[0];
+			if (max < arr[i]) {
+				max = arr[i];
 			}
 
 		}
@@ -64,8 +92,8 @@ public class Solution4 {
 		double min = arr[0];
 
 		for (int i = 0; i < arr.length; i++) {
-			if (min > arr[0]) {
-				min = arr[0];
+			if (min > arr[i]) {
+				min = arr[i];
 			}
 		}
 
@@ -77,14 +105,78 @@ public class Solution4 {
 		int min = arr[0];
 
 		for (int i = 0; i < arr.length; i++) {
-			if (min > arr[0]) {
-				min = arr[0];
+			if (min > arr[i]) {
+				min = arr[i];
 			}
 		}
 
 		return (double) min;
 	}
+	
+	
+	public static int idxMin(int[] arr) {
 
+		int idxMin = 0;
+		int min = arr[0];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (min > arr[i]) {
+				min = arr[i];
+				idxMin = i;
+			}
+		}
+
+		return idxMin;
+	}
+	
+	public static int idxMin(double[] arr) {
+
+		int idxMin = 0;
+		double min = arr[0];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (min > arr[i]) {
+				min = arr[i];
+				idxMin = i;
+			}
+		}
+
+		return idxMin;
+	}
+
+	
+	public static int idxMax(int[] arr) {
+
+		int idxMax = 0;
+		int max = arr[0];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+				idxMax = i;
+			}
+
+		}
+
+		return idxMax;
+	}
+	
+	public static int idxMax(double[] arr) {
+
+		int idxMax = 0;
+		double max = arr[0];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+				idxMax = i;
+			}
+
+		}
+
+		return idxMax;
+	}
+	
 	public static int[] divider(int[] arr, double denominator) {
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] /= denominator;
@@ -114,21 +206,30 @@ public class Solution4 {
 	}
 	
 	public static void printArray(double[] arr) {
+		System.out.println("Печать double");
 		for (int i = 0; i < arr.length; i++) {
-			System.out.print(" " + arr[0]);
+			System.out.print(" " + arr[i]);
 		}
+		System.out.println();
 	}
 	
 	public static void printArray(int[] arr) {
+		System.out.println("Печать int");
 		for (int i = 0; i < arr.length; i++) {
-			System.out.print(" " + arr[0]);
+			System.out.print(" " + arr[i]);
 		}
+		System.out.println();
 	}
 
-	public static double[] change(double[] arr,int[] arr1, int denominator) {
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] /= denominator;
-		}
-		return arr;
+	public static void change(int[] arr1, double[] arr2) {
+		
+		
+		double p_max2 = arr2[idxMax(arr2)];
+		
+		
+		arr2[idxMax(arr2)] = (double) arr1[idxMin(arr1)];
+		arr1[idxMin(arr1)] = (int) p_max2;
+		
+ 		
 	}
 }
